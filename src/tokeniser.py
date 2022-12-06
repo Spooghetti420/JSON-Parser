db106853bb1009ea3cc19742e17bdfa6692c739f
Spooghetti420 <kkovachev1947@gmail.com>
2022-12-06 17:51:47 +0000

    Convert tokeniser to a class

--- src/tokeniser.py
+++ src/tokeniser.py
@@ -6,55 +6,71 @@ from jtokens import Token, TokenTypes
 class JSONTokenError(ValueError):
     pass
 
-def __tokenise(json_str: str):
-    current: int = 0
-    start: int = 0
-    line: int = 0
-    tokens: list[Token] = []
 
-    def at_end():
-        return current >= len(json_str)
+class EscapeSequenceEnd(JSONTokenError):
+    pass
 
+
+class Tokeniser:
+    def __init__(self, json_str: str) -> None:
+        self.source = json_str
+        self.current: int = 0
+        self.start: int = 0
+        self.line: int = 0
+        self.col: int = 0
+        self.tokens: list[Token] = []
+        self.complete: bool = False
+
+    SYMBOLS = {
+        "{": TokenTypes.LEFT_CURLY_BRACKET,
+        "}": TokenTypes.RIGHT_CURLY_BRACKET,
+        "[": TokenTypes.LEFT_SQUARE_BRACKET,
+        "]": TokenTypes.RIGHT_SQUARE_BRACKET,
+        ",": TokenTypes.COMMA,
+        ":": TokenTypes.COLON  
+    }
+
+    def at_end(self):
+        return self.current >= len(self.source)
+
+    @staticmethod
     def is_num(x: str) -> bool:
         return "0" <= x <= "9"
 
+    @staticmethod
     def is_letter(x: str) -> bool:
         return "A" <= x <= "z"
 
-    def add_token(token_type: TokenTypes, literal: Optional[Any] = None):
-        nonlocal start
-        tokens.append(Token(current, token_type, json_str[start:current], literal))
-        start = current
+    def add_token(self, token_type: TokenTypes, literal: Optional[Any] = None):
+        self.tokens.append(Token(self.line, self.col, token_type, self.source[self.start:self.current], literal))
+        self.start = self.current
 
-    def subsume(value_type: Callable, limit: int = None) -> str:
-        nonlocal current
+    def subsume(self, value_type: Callable, limit: int = None) -> str:
         output = ""
         i = 0
-        while value_type((t := json_str[current])) and (((limit is not None) and i < limit) or limit is None):
+        while value_type((t := self.source[self.current])) and (((limit is not None) and i < limit) or limit is None):
             output += t
-            current += 1
+            self.current += 1
 
         return output
 
-    def consume_until(end_value: str) -> None:
+    def consume_until(self, end_value: str) -> None:
         # Continually increments `current` until it is equal to the given "end value".
-        nonlocal current
-        while json_str[current:current+len(end_value)] != end_value:
-            current += 1
+        while self.source[self.current:self.current+len(end_value)] != end_value:
+            self.current += 1
+        
         return
 
-            
-    def parse_string() -> str:
-        """Given a starting quotation mark, finds an ending quotation mark and the contained text."""
-        nonlocal current
+    def parse_string(self) -> str:
+        """Given a starting quotation mark, finds an ending quotation mark and returns a string with the contained text."""
         content = ""
 
-        while not at_end():
-            t = json_str[current]
+        while not self.at_end():
+            t = self.source[self.current]
             if t == "\\":
                 try:
-                    current += 1
-                    next_t = json_str[current]
+                    self.current += 1
+                    next_t = self.source[self.current]
                     if next_t in ('"', "\\", "/"):
                         content += next_t
                     elif next_t == "b":
@@ -69,48 +85,46 @@ def __tokenise(json_str: str):
                         content += "\t"
                     elif next_t == "u":
                         # Unicode escape sequence expected
-                        next_four = json_str[current+1:current+5]
-                        current += 4
+                        next_four = self.source[self.current+1:self.current+5]
+                        self.current += 4
                         if len(next_four) != 4:
-                            raise IndexError()
+                            raise EscapeSequenceEnd(f"Escape sequence (\\u{next_four}) was triggered at the end of the JSON input with insufficient characters")
                         content += bytes(next_four, "ascii").decode("unicode-escape")
 
                 except IndexError:
-                    # print(f"String abruptly terminated at {line} col {start}")
-                    raise SystemExit(1)
+                    raise EscapeSequenceEnd("")
 
             elif t == '"':
-                current += 1
+                self.current += 1
                 return content
             else:
                 content += t
 
-            current += 1
-    
-    def parse_num() -> float:
-        nonlocal current
-        current -= 1
+            self.current += 1
+
+    def parse_num(self) -> float:
+        self.current -= 1
 
         content = "" # String representation of the number being parsed
         
-        content += subsume(is_num) # Add a series of numbers to the string
-        decimal = subsume(lambda x: x == ".", limit=1) # Potentially add a decimal point to the string
+        content += self.subsume(self.is_num) # Add a series of numbers to the string
+        decimal = self.subsume(lambda x: x == ".", limit=1) # Potentially add a decimal point to the string
         if decimal != "":
             content += decimal
-            fractional_part = subsume(is_num) # If decimal part exists, find any fractional part after the point
+            fractional_part = self.subsume(self.is_num) # If decimal part exists, find any fractional part after the point
             content += fractional_part
 
         # Try to find an 'e' for exponential notation
-        e = subsume(lambda x: x == "e", limit=1)
+        e = self.subsume(lambda x: x == "e", limit=1)
         if e != "":
             content += "e"
-            sign = subsume(lambda s: s in ("+", "-"), limit=1)
+            sign = self.subsume(lambda s: s in ("+", "-"), limit=1)
             content += sign
-            exponent = subsume(is_num)
+            exponent = self.subsume(self.is_num)
             if exponent:
                 content += exponent
             else:
-                raise JSONTokenError(f"Number literal {json_str[start:current+1]} contains exponent notation with invalid or absent subsequent exponent.")
+                raise JSONTokenError(f"Number literal {self.source[self.start:self.current+1]} contains exponent notation with invalid or absent subsequent exponent.")
         
         num_float = float(content)
         if floor(num_float) - num_float < 1e-6:
@@ -118,50 +132,48 @@ def __tokenise(json_str: str):
         else:
             return num_float
 
-    # Tokenising begins here
-    while not at_end():
-        t = json_str[current]
-        current += 1
+    def tokenise(self):
+        if self.complete:
+            return self.tokens
 
-        if t.isspace():
-            if t == "\n":
-                line += 1
-            start = current
-        elif t == "{":
-            add_token(TokenTypes.LEFT_CURLY_BRACKET)
-        elif t == "}":
-            add_token(TokenTypes.RIGHT_CURLY_BRACKET)
-        elif t == "[":
-            add_token(TokenTypes.LEFT_SQUARE_BRACKET)
-        elif t == "]":
-            add_token(TokenTypes.RIGHT_SQUARE_BRACKET)
-        elif t == ",":
-            add_token(TokenTypes.COMMA)
-        elif t == ":":
-            add_token(TokenTypes.COLON)
-        elif t == '"':
-            add_token(TokenTypes.STRING, parse_string())
-        elif is_num(t):
-            add_token(TokenTypes.NUMBER, parse_num())
-        elif t == "/":
-            if json_str[current] == "/":
-                # We have a comment; these are not permitted in JSON, but I want to include them!
-                consume_until("\n")
-                current += 1
-            elif json_str[current] == "*":
-                # Multiline comment, in fact!
-                consume_until("*/")
-                current += 1
-        elif is_letter(t):
-            current -= 1
-            word = subsume(is_letter)
-            if word == "true":
-                add_token(TokenTypes.TRUE, True)
-            elif word == "false":
-                add_token(TokenTypes.FALSE, False)
-            elif word == "null":
-                add_token(TokenTypes.NULL, None)
+        # Tokenising begins here
+        while not self.at_end():
+            t = self.source[self.current]
+            self.current += 1
 
-    add_token(TokenTypes.EOF)
+            if t.isspace():
+                if t == "\n":
+                    self.line += 1
+                self.start = self.current
+            elif t in Tokeniser.SYMBOLS:
+                self.add_token(Tokeniser.SYMBOLS[t])
+            elif t == '"':
+                self.add_token(TokenTypes.STRING, self.parse_string())
+            elif self.is_num(t):
+                self.add_token(TokenTypes.NUMBER, self.parse_num())
+            elif t == "/":
+                if self.source[self.current] == "/":
+                    # We have a comment; these are not permitted in JSON, but I want to include them!
+                    self.consume_until("\n")
+                    self.current += 1
+                elif self.source[self.current] == "*":
+                    # Multiline comment, in fact!
+                    self.consume_until("*/")
+                    self.current += 1
+            elif self.is_letter(t):
+                self.current -= 1
+                word = self.subsume(self.is_letter)
+                if word == "true":
+                    self.add_token(TokenTypes.TRUE, True)
+                elif word == "false":
+                    self.add_token(TokenTypes.FALSE, False)
+                elif word == "null":
+                    self.add_token(TokenTypes.NULL, None)
 
-    return tokens
+        self.add_token(TokenTypes.EOF)
+
+        self.complete = True
+        return self.tokens
+        
+def __tokenise(json_str: str):
+    return Tokeniser(json_str).tokenise()
